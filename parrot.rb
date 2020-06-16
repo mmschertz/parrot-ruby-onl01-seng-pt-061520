5ae4ac433a33e09591a61ecac1cdfaca959a15e0
Megan Schertz <meganschertz@gmail.com>
2020-06-16 20:16:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string="Squawk!")
+def parrot(phrase="Squawk!")
     puts "#{string}"
     "#string"
   end
